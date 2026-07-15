c228952c8a019661df8790c54acabc7cec864ece
s3e0o <143053347+s3e0o@users.noreply.github.com>
2026-07-15 12:00:13 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Flower Shop</h1>
+    </main>
+  );
+}
+
+export default App;
